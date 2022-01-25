2dbc868648f7179cae6d204ddc4b473d086f220e
krmvmaria <krmvmaria@mail.ru>
2022-01-25 19:53:31 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getName());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getName());
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
